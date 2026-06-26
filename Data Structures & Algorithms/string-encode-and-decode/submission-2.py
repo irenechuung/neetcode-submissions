dde72e0e8acc49abf87a4398d7f7b573e59a5e10
irenechuung <91218417+irenechuung@users.noreply.github.com>
2026-06-25 22:17:32 -0700

    Add: string-encode-and-decode - submission-2

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-2.py
@@ -0,0 +1,15 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        en=""
+        for i in range(0,len(strs)):
+            en+=strs[i]
+            if i!=len(strs)-1:
+                en+="_"
+        return en
+
+    def decode(self, s: str) -> List[str]:
+        list=s.split("_")     
+        if len(list)==0:
+            return []
+        return list
