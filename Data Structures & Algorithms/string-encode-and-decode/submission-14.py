9181e1e7638ed228fc82ae4b31176b0ff9490955
irenechuung <91218417+irenechuung@users.noreply.github.com>
2026-06-28 23:30:34 -0700

    Add: string-encode-and-decode - submission-14

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-14.py
@@ -0,0 +1,28 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        en=""
+        for i in range(0,len(strs)):
+            if strs[i]!="_":
+                en+=strs[i]
+            elif (strs[i]=="" and i==len(strs)-1):
+                en+="meow"
+                en+="_"
+            elif strs[i]=="":
+                en+="meow"
+            else:
+                en+="&"
+            if i!=len(strs)-1:
+                en+="_"
+        return en
+
+    def decode(self, s: str) -> List[str]:
+        list=s.split("_")     
+        for i in range(0,len(list)):
+            if(list[i]=="meow"):
+                list[i]==""
+            elif(list[i]=="&"):
+                list[i]=_
+        if(list==[""]):
+            return []
+        return list
