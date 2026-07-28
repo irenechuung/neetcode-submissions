a0e8b6df1c2f3db975667d9d5d748e9fa36b4f02
irenechuung <91218417+irenechuung@users.noreply.github.com>
2026-07-27 17:52:16 -0700

    Add: validate-parentheses - submission-1

---
+++ Data Structures & Algorithms/validate-parentheses/submission-1.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+           
+            # go through every character in string for each one
+            # check if it's in the stack 
+            # add to stack as long as the element isn't in the stack 
+            # if it is then pop the stack and check that its = to what was popped
+                # if not => return false
+            # after all char's are checked => return true 
+        
+        stack = []
+        map = {"{": "}","}": "{", "(": ")", ")": "(", "[": "]", "]": "["}
+        for c in s:
+            if(map[c] in stack):
+                popped=stack.pop()
+                if popped != map[c]:
+                    return False
+            else:
+                stack.append(c)
+        return True
