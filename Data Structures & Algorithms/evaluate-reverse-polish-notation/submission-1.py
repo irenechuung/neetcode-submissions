e8f775fe496444d89072ffdae9787e4bba6a912d
irenechuung <91218417+irenechuung@users.noreply.github.com>
2026-07-28 18:00:20 -0700

    Add: evaluate-reverse-polish-notation - submission-1

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-1.py
@@ -0,0 +1,33 @@
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        '''
+        have an empty stack
+        have an output int array 
+        as long as it's not an operator (+, -, *, /) push to stack 
+            when u hit an operator then u want to execute the operation 
+            make sure u reverse it cause stack is last in first out 
+                top = first pop
+                bottom = second pop 
+                
+                bottom operator top 
+        '''
+
+        stack = []
+        output = 0
+        for part in tokens:
+            if(part not in "+-*/"):
+                stack.append(part)
+            else:
+                piece = 0
+                top = stack.pop()
+                bottom = stack.pop()
+                if(part == "+"):
+                    piece = int(bottom) + int(top)
+                elif(part == "-"):
+                    piece = int(bottom) - int(top)
+                elif(part == "*"):
+                    piece = int(bottom) * int(top)
+                else:
+                    piece = int(int(bottom)/int(top))
+                stack.append(piece)
+        return stack[-1]
