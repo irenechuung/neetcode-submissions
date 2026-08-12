8facffe75b453c8a2930db246daff93efeaac7d1
irenechuung <91218417+irenechuung@users.noreply.github.com>
2026-08-12 13:34:13 -0700

    Add: find-target-in-rotated-sorted-array - submission-1

---
+++ Data Structures & Algorithms/find-target-in-rotated-sorted-array/submission-1.py
@@ -0,0 +1,35 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+
+        '''
+        thought process
+            don't think should check adjacent elemernts 
+            instead:
+                can compare against left and rihg pointer 
+                if (curr > nums[l] and curr > target) and :
+                    check right side 
+                if curr > nums[l] and curr < target:
+                    check left side 
+                if curr < nums[r] and nums[r]<target:
+                    check left side 
+                if curr < nums[r] and nums[r]>target:
+                    check right side 
+
+                    
+        '''
+        l = 0
+        r = len(nums)-1
+    
+        while l<=r:
+            mid = (l+r)//2
+            curr = nums[mid]
+            x = (curr > nums[l] and curr > target) or (curr < nums[r] and nums[r]>target)
+            y = (curr > nums[l] and curr < target) or (curr < nums[r] and nums[r]<target)
+            if curr == target:
+                return mid
+            if x:
+                l = mid
+            else:
+                r=mid-1
+            
+        return -1
